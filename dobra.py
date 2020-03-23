9e6e433a3e8bf5f97d28277bd933a79e0594c7f5
Mateusz <maciaszek96@gmail.com>
2020-03-23 01:33:06 +0100

    delete unnecessary data and analysis

--- dobra.py
+++ dobra.py
@@ -4,50 +4,21 @@ import matplotlib.pyplot as plt
 from scipy import stats
 
 alpha = 0.05
+manaus_level_hypothesis=0
 
-df1 = pandas.read_table('data/4/Births.csv', sep=',', index_col=0)
-print(f"births mean {df1.births.mean()}")
-res1=stats.ttest_1samp(df1.births, 10000)
-print(f"birth p-value: {res1.pvalue}")
-if res1.pvalue > alpha:
-    print("birth hypothesis is true")
+print(f"testing following hypothesis: \n"
+      f"mean level of manaus river equal to: {manaus_level_hypothesis}\n"
+      f"statistical significance: {alpha}")
+df = pandas.read_table('data/4/manaus.csv', sep=',', index_col=0)
+res = stats.ttest_1samp(df.manaus, manaus_level_hypothesis)
+print(f"T-student test conducted on the data returned p-value: {res.pvalue}")
+if res.pvalue > alpha:
+    print("p-value is greater than the statistical significance, therefore the hypothesis is true")
 else:
-    print("birth hypothesis is false")
+    print("p-value is smaller than the statistical significance, therefore the hypothesis is false")
 print('\n')
 
-df2 = pandas.read_table('data/4/manaus.csv', sep=',', index_col=0)
-print(f"manaus mean: {df2.manaus.mean()}")
-res2=stats.ttest_1samp(df2.manaus, 0)
-print(f"manaus p-value: {res2.pvalue}")
-if res2.pvalue > alpha:
-    print("manaus hypothesis is true")
-else:
-    print("manaus hypothesis is false")
-print('\n')
-
-df3 = pandas.read_table('data/4/quakes.csv', sep=',', index_col=0)
-print(f"quakes mean: {df3.depth.mean()}")
-res3=stats.ttest_1samp(df3.depth, 300)
-print(f"quakes p-value: {res3.pvalue}")
-if res3.pvalue > alpha:
-    print("quakes hypothesis is true")
-else:
-    print("quakes hypothesis is false")
-
-fig = plt.figure()
-ax1 = fig.add_subplot(2,2,1)
-sns.distplot(df1.births, color="blue", label="births number", hist_kws={"align": "mid"})
-plt.axvline(10000, color='k', linestyle='dashed', linewidth=1)
-ax2 = fig.add_subplot(2,2,2)
-sns.distplot(df2.manaus, color="blue", label="Manaus height")
+sns.distplot(df.manaus, color="blue", label="Manaus level")
 plt.axvline(0, color='k', linestyle='dashed', linewidth=1)
-ax3 = fig.add_subplot(2,2,3)
-sns.distplot(df3.depth, color="blue", label="quakes depth")
-plt.axvline(300, color='k', linestyle='dashed', linewidth=1)
 plt.legend()
 plt.show()
-
-# todo:
-# 2. Zwizualizować rozkłady na histogramie.
-# 3. Zaznaczyć na wykresie punkt dotyczący badanej hipotezy.
-# Należy zweryfikować normalność rozkładów, jeśli to konieczne.
