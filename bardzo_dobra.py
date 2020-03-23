b5715f4db0602e8df011e3e6959cf421c2d221ea
krzys <krzysztof.krzeszewski@tdsoft.pl>
2020-03-23 12:33:34 +0100

    factorize on a single column and filtering only needed columns

--- bardzo_dobra.py
+++ bardzo_dobra.py
@@ -2,9 +2,9 @@ import pandas
 
 df = pandas.read_table('data/5/footballers.csv', sep=';')
 
-#retaining only 2 positions
-df = df.loc[df['Position'].isin(['Relief_Pitcher','Starting_Pitcher'])]
-# df_factor = df.apply(lambda x: x.factorize()[0])
-# print(df_factor)
+# retaining only 2 positions and only important information (position and weight)
+df = df.filter(["Weight(pounds)", "Position"]).loc[df['Position'].isin(['Relief_Pitcher', 'Starting_Pitcher'])]
+df.Position = df.Position.factorize()[0]
+print(df)
 correl = df.corr("spearman")
 print(correl)
