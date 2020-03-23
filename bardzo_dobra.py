f40e599d3de59fd228b7541139ac0581d9e64233
krzys <krzysztof.krzeszewski@tdsoft.pl>
2020-03-23 13:22:52 +0100

    dual graphs on all positions

--- bardzo_dobra.py
+++ bardzo_dobra.py
@@ -1,10 +1,23 @@
 import pandas
+import seaborn as sns
+from matplotlib import pyplot as plt
+import numpy as np
 
 df = pandas.read_table('data/5/footballers.csv', sep=';')
 
 # retaining only 2 positions and only important information (position and weight)
-df = df.filter(["Weight(pounds)", "Position"]).loc[df['Position'].isin(['Relief_Pitcher', 'Starting_Pitcher'])]
-df.Position = df.Position.factorize()[0]
-print(df)
-correl = df.corr("spearman")
+df = df.filter(["Weight(pounds)", "Position"]) #.loc[df["Position"].isin(['Relief_Pitcher', 'Starting_Pitcher'])]
+factorizedPositions = df.Position.factorize()
+df.Position = factorizedPositions[0]
+
+correl = df.corr(method="spearman")
 print(correl)
+
+for column in df.columns:
+    sns.distplot(df[column], label=f"{column}", axlabel=False)
+
+plt.figure(figsize=(5, 2))
+plt.scatter(df["Weight(pounds)"], df.Position)
+plt.yticks(np.arange(len(factorizedPositions[1])), factorizedPositions[1])
+
+plt.show()
