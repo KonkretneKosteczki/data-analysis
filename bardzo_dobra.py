f3b16a1e36dd2bbdeb770e9b9d983d27910c53c7
Mateusz <maciaszek96@gmail.com>
2020-03-23 12:14:17 +0100

    retaining only 2 positions in analyzed data

--- bardzo_dobra.py
+++ bardzo_dobra.py
@@ -1,10 +1,10 @@
 import pandas
-import seaborn as sns
-import matplotlib.pyplot as plt
-from scipy import stats
-from scipy.stats import spearmanr
 
 df = pandas.read_table('data/5/footballers.csv', sep=';')
-df_factor = df.apply(lambda x: x.factorize()[0])
+
+#retaining only 2 positions
+df = df.loc[df['Position'].isin(['Relief_Pitcher','Starting_Pitcher'])]
+# df_factor = df.apply(lambda x: x.factorize()[0])
+# print(df_factor)
 correl = df.corr("spearman")
 print(correl)
