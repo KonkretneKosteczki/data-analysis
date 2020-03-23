cbd5118735d430643911fed062736d394c1e8902
Mateusz <maciaszek96@gmail.com>
2020-03-23 15:58:12 +0100

    add scipy solution for 5

--- bardzo_dobra.py
+++ bardzo_dobra.py
@@ -1,12 +1,28 @@
 import pandas
 import seaborn as sns
 from matplotlib import pyplot as plt
+
 import numpy as np
+from scipy.stats import spearmanr
 
 df = pandas.read_table('data/5/footballers.csv', sep=';')
 
 # retaining only 2 positions and only important information (position and weight)
 df = df.filter(["Weight(pounds)", "Position"]).loc[df["Position"].isin(['Relief_Pitcher', 'Starting_Pitcher'])]
+
+#using scipy
+alpha = 0.05
+
+rho, pval = spearmanr(df["Weight(pounds)"], df["Position"])
+print("statistical significance: ",alpha)
+print("rho(correlation coefficient): ",rho)
+print("pval: ", pval)
+if pval>alpha:
+    print("the results are statistically significant")
+else:
+    print("the results are not statistically significant")
+
+#using pandas
 factorizedPositions = df.Position.factorize()
 factorizedPositionsLabels = factorizedPositions[1]
 df.Position = factorizedPositions[0]
