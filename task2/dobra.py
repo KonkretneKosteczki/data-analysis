fa229de72c04d74eb4d749c4f807fe781da13c33
Mateusz <maciaszek96@gmail.com>
2020-04-26 14:06:28 +0200

    add part for grade 4 in task 2

---
+++ task2/dobra.py
@@ -0,0 +1,46 @@
+import math
+import numpy as np
+import pandas as pd
+
+from task2.dostateczna import read_data, regression_curve_before, compare_data, regression_curve_after
+
+
+def interpolation_imputation(data):
+    filled = pd.DataFrame()
+    for (columnName, columnData) in data.iteritems():
+        filled[columnName] = columnData.fillna(columnData.interpolate())
+    return filled
+
+def hot_deck_imputation(data):
+    return data.fillna(method="ffill") #LOCF
+
+def regression_curve_imputation(adata, model):
+    data = adata.copy()
+    data.loc[(pd.isnull(data[x_column])), x_column] = data[y_column]/model.coef_-model.intercept_
+    return data
+
+
+if __name__ == '__main__':
+    # 1. Wszystkie wymagania na ocenę dostateczną.
+    initial_data = read_data()
+    x_column = "TOEFL_Score" #missing data
+    y_column = "CGPA"
+    model, r_sq = regression_curve_before(initial_data, x_column, y_column)
+    # 2. Uzupełnić braki metodą interpolacji, hot-deck, oraz wartościami uzyskanymi z krzywej regresji wyznaczonej na podstawie danych bez braków.
+    filled_interpolation = interpolation_imputation(initial_data)
+    filled_hot_deck = hot_deck_imputation(initial_data)
+    filled_regression=regression_curve_imputation(initial_data, model)
+    # 3. Porównać charakterystyki zbiorów przed i po imputacji (średnia, odchylenie standardowe, kwartyle).
+    print("INTERPOLATION")
+    compare_data(initial_data, filled_interpolation, x_column, y_column)
+    print("HOT-DECK")
+    compare_data(initial_data, filled_hot_deck, x_column, y_column)
+    print("REGRESSION")
+    compare_data(initial_data, filled_regression, x_column, y_column)
+    # 4. Wyznaczyć krzywą regresji dla danych po każdej imputacji. Porównać jak zmieniły się parametry krzywych.
+    print("INTERPOLATION")
+    regression_curve_after(filled_interpolation, model, r_sq, x_column, y_column)
+    print("HOT-DECK")
+    regression_curve_after(filled_hot_deck, model, r_sq, x_column, y_column)
+    print("REGRESSION")
+    regression_curve_after(filled_regression, model, r_sq, x_column, y_column)
--- task2/dostateczna.py
+++ task2/dostateczna.py
@@ -44,7 +44,7 @@ def regression_curve_before(data, x_column, y_column):
     x_data = data_without_nans[[x_column]]
     y_data = data_without_nans[y_column]
 
-    model = LinearRegression().fit(x_data, y_data)
+    model = LinearRegression().fit(x_data, y_data) # takes: X{array-like, sparse matrix} of shape (n_samples, n_features);yarray-like of shape (n_samples,) or (n_samples, n_targets)
     r_sq = model.score(x_data, y_data)
     print("R^2:", r_sq)
     print("intercept:", model.intercept_)
@@ -102,3 +102,20 @@ def regression_curve_after(data, model, r_sq, x_column, y_column):
     print()
     create_graph(model_2.coef_, model_2.intercept_, x_data, y_data, "Graph of linear regression after", x_column,
                  y_column)
+
+if __name__ == '__main__':
+    # Wybrać zbiór danych do analizy, korzystając z dowolnego repozytorium danych (UCI Repository, Kaggle, inne). W zbiorze powinny występować brakujące dane, jednak nie powinno być ich zbyt wiele (około 5%-10%). Proszę również zwrócić uwagę, że dalsza część poleceń uwzględnia regresję liniową, która jest stosowana dla skal ilościowych (i zasadniczo zakłada zgodność z rozkładem normalnym). Proszę zatem zadbać, żeby parametry, które podlegają regresji liniowej miały charakterystykę pozwalającą na zaobserwowanie różnic (czyli chociażby charakteryzowały się zauważalną wariancją).
+    x_column = "TOEFL_Score"
+    y_column = "CGPA"
+
+    # Wczytać dane z brakami, policzyć jaki procent danych zawiera braki.
+    data = read_data()
+    percent(data)
+    # Wyznaczyć krzywą regresji dla danych bez braków.
+    model, r_sq = regression_curve_before(data, x_column, y_column)
+    # Uzupełnić braki metodą "mean imputation"
+    filled = mean_imputation(data)
+    # Porównać charakterystykę zbiorów przed i po imputacji (średnia, odchylenie standardowe, kwartyle).
+    compare_data(data, filled, x_column, y_column)
+    # Wyznaczyć krzywą regresji dla danych po imputacji. Porównać jak zmieniły się parametry krzywej
+    regression_curve_after(filled, model, r_sq, x_column, y_column)
