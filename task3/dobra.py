f4d621328471da5c670dd8777061f8437a98e8ea
Mateusz <maciaszek96@gmail.com>
2020-05-31 19:16:14 +0200

    add selection with chi2, add training and plotting to 3 and 4 mark tasks

--- task3/dobra.py
+++ task3/dobra.py
@@ -1,24 +1,50 @@
 import pandas as pd
+from sklearn import model_selection
+from sklearn.feature_selection import chi2, SelectKBest
 
 # 1. Wszystkie wymagania na ocenę dostateczną oraz dodatkowo:
-dataset_path = 'data/iris.data'
-dataset_headers = ["sepal_length", "sepal_width", "petal_length","petal_width","class"]
+from sklearn.linear_model import SGDClassifier
+import matplotlib.pyplot as plt
+from task3.dostateczna import train, read_data
 
-df = pd.read_csv(dataset_path,
-                 header=None,
-                 names=dataset_headers)
-X = df.iloc[:,:4] #training data
-y = df.iloc[:,4]  #labels
+if __name__ == '__main__':
+    df = read_data()
+    X = df.iloc[:,:4] #training data
+    y = df.iloc[:,4]  #labels
 
-# 2. Dokonać wyboru cech pozostawiając tylko 2, a odrzucając cechy o a) największej wariancji, b) najmniejszej wariancji
+    # 2. Dokonać wyboru cech pozostawiając tylko 2, a odrzucając cechy o a) największej wariancji, b) najmniejszej wariancji
 
-# print(df.var())
-low_to_high_var_series = X.var().sort_values()
-low_var_indices = low_to_high_var_series.head(2).keys()
-high_var_indices = low_to_high_var_series.tail(2).keys()
-df_low_var = X[low_var_indices]
-df_high_var = X[high_var_indices]
+    # print(df.var())
+    low_to_high_var_series = X.var().sort_values()
+    low_var_indices = low_to_high_var_series.head(2).keys()
+    high_var_indices = low_to_high_var_series.tail(2).keys()
+    X_low_var = X[low_var_indices]
+    X_high_var = X[high_var_indices]
+    # 3. Na zredukowanej liczbie cech przeprowadzić klasyfikację klasyfikatorem z wymagań na ocenę dostateczną.
+    X_train_l, X_test_l, y_train_l, y_test_l = model_selection.train_test_split(X_low_var, y)
+    sgd_l = SGDClassifier()
+    print("\nTraining SGDClassifier for low variance features")
+    training_data_l = train(sgd_l, X_train_l, y_train_l, X_test_l, y_test_l, 1000)
+    plt.plot(*training_data_l)
+    plt.show()
 
-# 3. Na zredukowanej liczbie cech przeprowadzić klasyfikację klasyfikatorem z wymagań na ocenę dostateczną.
-# 4. Eksperyment powtórzyć wykorzystując do selekcji test niezależności chi^2.
-# 5. Wykonać odpowiednie wykresy.
+    X_train_h, X_test_h, y_train_h, y_test_h = model_selection.train_test_split(X_high_var, y)
+    sgd_h = SGDClassifier()
+    print("\nTraining SGDClassifier for high variance features")
+    training_data_h = train(sgd_h, X_train_h, y_train_h, X_test_h, y_test_h, 1000)
+    plt.plot(*training_data_h)
+    plt.show()
+
+    # 4. Eksperyment powtórzyć wykorzystując do selekcji test niezależności chi^2.
+    chi2selector = SelectKBest(score_func=chi2, k=2)
+    chi2selector.fit_transform(X,y)
+    selected_col_numbers = chi2selector.get_support(indices=True)
+    X_chi2 = df.iloc[:,selected_col_numbers]
+
+    # 5. Wykonać odpowiednie wykresy.
+    X_train_ch, X_test_ch, y_train_ch, y_test_ch = model_selection.train_test_split(X_chi2, y)
+    sgd_ch = SGDClassifier()
+    print("\nTraining SGDClassifier for high variance features")
+    training_data_ch = train(sgd_ch, X_train_ch, y_train_ch, X_test_ch, y_test_ch, 1000)
+    plt.plot(*training_data_ch)
+    plt.show()
--- task3/dostateczna.py
+++ task3/dostateczna.py
@@ -26,36 +26,39 @@ def train(model: SGDClassifier, train_data, train_labels, test_data, test_labels
 
     return labels, scores
 
+def read_data():
+    dataset_path = 'data/iris.data'
+    dataset_headers = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
 
-dataset_path = 'data/iris.data'
-dataset_headers = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
+    df = pd.read_csv(dataset_path,
+                     header=None,
+                     names=dataset_headers)
+    return df
 
-df = pd.read_csv(dataset_path,
-                 header=None,
-                 names=dataset_headers)
-X = df.iloc[:, :4]
-y = df.iloc[:, 4]
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y)
+if __name__ == '__main__':
+    df = read_data()
+    X = df.iloc[:, :4]
+    y = df.iloc[:, 4]
+    X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y)
 
-# 1. Przeprowadzić klasyfikację za pomocą wybranej techniki klasyfikacji (np. MLP, SVM, SGD, etc.)
-sgd = SGDClassifier()
-print("Training SGDClassifier")
-training_data = train(sgd, X_train, y_train, X_test, y_test, 1000)
+    # 1. Przeprowadzić klasyfikację za pomocą wybranej techniki klasyfikacji (np. MLP, SVM, SGD, etc.)
+    sgd = SGDClassifier()
+    print("Training SGDClassifier")
+    training_data = train(sgd, X_train, y_train, X_test, y_test, 1000)
 
-# 2. Wykonać wykres z przedstawioną skutecznością klasyfikatora
-# (procent poprawnie sklasyfikowanych próbek) od liczby iteracji/epok.
-plt.plot(*training_data)
-plt.show()
+    # 2. Wykonać wykres z przedstawioną skutecznością klasyfikatora
+    # (procent poprawnie sklasyfikowanych próbek) od liczby iteracji/epok.
+    plt.plot(*training_data)
+    plt.show()
 
-# 3. Dokonać analizy głównych składowych (PCA) i zredukować liczbę cech do 2 składowych.
-pca = PCA(n_components=2)
-X_new = pca.fit_transform(X)
+    # 3. Dokonać analizy głównych składowych (PCA) i zredukować liczbę cech do 2 składowych.
+    pca = PCA(n_components=2)
+    X_new = pca.fit_transform(X)
 
-# 4. Ponownie przeprowadzić klasyfikację i porównać wyniki.
-X_train2, X_test2, y_train2, y_test2 = model_selection.train_test_split(X_new, y)
-sgd2 = SGDClassifier(max_iter=1000)
-sgd2.fit(X_train2, y_train2)
-y_pred2 = sgd2.predict(X_test2)
-# res = sgd2.score(X_test2, y_test2)
-res = accuracy_score(y_test2, y_pred2)
-print(res)
+    # 4. Ponownie przeprowadzić klasyfikację i porównać wyniki.
+    X_train2, X_test2, y_train2, y_test2 = model_selection.train_test_split(X_new, y)
+    sgd2 = SGDClassifier()
+    print("\nTraining SGDClassifier after PCA")
+    training_data2 = train(sgd2, X_train2, y_train2, X_test2, y_test2, 1000)
+    plt.plot(*training_data2)
+    plt.show()
